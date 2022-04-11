0d5c1965e7594cd063b9db2fa276ce9a917504d6
SamiNakouzi <sami_nakouzi@hotmail.com>
2022-04-11 23:33:02 +0200

    fixed perturbation bug due to adaptive odeint

--- script/energy_th.py
+++ script/energy_th.py
@@ -6,9 +6,6 @@ from matplotlib import style
 from scipy.integrate import odeint
 from sim import yamada_model
 
-
-
-
 inv_dmu = []
 temp = []
 
@@ -19,7 +16,8 @@ model = yamada_model()
 
 for i in range(len(dt)):
     for p in np.arange(0, 1.1 ,0.001):
-        model.integrate(t, dt[i], p) #(time, duration, perturbation amplitude)
+        model.perturbate(dt[i], p)
+        model.integrate(t) 
         x = model.sol.tolist()
         I = []
         for j in range(0, len(t)):
@@ -31,21 +29,20 @@ for i in range(len(dt)):
         else:
             continue
         
-        
-
 for i in range(len(temp)):
     if inv_dmu[i] == inv_dmu[i+1]:
         threashold = temp[i]
         break
+
 #Ploting:
 
 style.use("seaborn")
-plt.scatter(temp, inv_dmu,color = 'r', label = "dots")
+plt.scatter(temp, inv_dmu, color = 'r', label = "dots")
 plt.vlines(threashold, color = 'blue',ymin = min(inv_dmu), ymax = max(inv_dmu), ls = '--')
 plt.plot(temp, inv_dmu, '--k', alpha = 0.5)
 plt.xlabel('$\Delta t$')
 plt.ylabel('$\dfrac{1}{\delta \mu}$', rotation=0, fontsize=12, labelpad=20)
 plt.text(threashold, min(inv_dmu)-0.1, '$\Delta t = 278.26$')
-#plt.legend()
+plt.legend()
 plt.title('$\dfrac{1}{\delta \mu} Vs \Delta t$')
 plt.show()
--- script/gain_intensity.py
+++ script/gain_intensity.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import math
 import scipy as sp
@@ -7,48 +6,65 @@ from matplotlib import style
 from scipy.integrate import odeint
 from sim import yamada_model
 
+# Linear scale time defimition
 t = np.linspace(0, 2000, 4000)
-dt = [80, 130, 500, 600]
-netgain = []
-Intensity = []
-G=[]
-model = yamada_model(mu1 = 2.43)
 
+# Duration of perturbation
+dt = [80, 130, 500, 600]
+
+# Netfain and Internsity per simulation 
+netgain, Intensity, G = [], [], []
+
+# Perturbation array for every perturbation duration 
+pert = np.zeros((4000, len(dt)))
+
+# Initiate Yamda simulator 
+model1 = yamada_model(mu1 = 2.43)
+
+# iterate over the simulation
 for i in range(len(dt)):
-    model.integrate(t, dt[i], 0.7) #(time, duration, perturbation amplitude)
+    # ALWAYS CALL PERTURBATE (IF NOT NEEDED, esp=0)
+    model.perturbate(dt[i], 0.7)
+    model.integrate(t) 
+
+    pert[:,i] = np.array(model.pert(t))
     x = model.sol.tolist()
-    I = []
-    ng = []
-    g=[]
+
+    # Netgain and Intensity per time interval
+    I, ng = [], []
     for j in range(0, 4000):
         ng.append(x[j][0] - (x[j][1] - 1))
-        #g.append(x[j][0])
         I.append(x[j][2])
+    
     netgain.append(ng)
     Intensity.append(I)
-    #G.append(g)
-    
-    
-#PLOTTING:
+ 
+
 fig, axs = plt.subplots(4, 2, sharex = True)
 axs[0, 0].plot(t, netgain[0],color = 'r', label = '$\Delta t = 5$')
 axs[0, 0].text(1500, 1.6, '$\Delta t = 80$', fontsize = 10)
 axs[0, 0].text(30, 1.7, '$\mu_1 = 2.43$', fontsize = 10)
 axs[0, 0].text(750, 1.68, 'Net Gain', fontsize = 10)
-axs[0, 1].plot(t, Intensity[0],color = 'r')
+
+axs[0, 1].plot(t, Intensity[0], color = 'r')
 axs[0, 1].text(750, 0.00056, 'Intensity', fontsize = 10)
+axs[0, 1].plot(t, pert[:,0], color='g')
 
 axs[1, 0].plot(t, netgain[1],color = 'green', label = '$\Delta t = 20$')
 axs[1, 1].plot(t, Intensity[1], color = 'green')
 axs[1, 0].text(1500, 1.65, '$\Delta t = 130$', fontsize = 10)
+axs[1, 1].plot(t, pert[:,1], color='g')
 
 axs[2, 0].plot(t, netgain[2],color = 'blue', label = '$\Delta t = 500$')
 axs[2, 1].plot(t, Intensity[2],color = 'blue')
 axs[2, 0].text(1500, 2.5, '$\Delta t = 500$', fontsize = 10)
+axs[2, 1].plot(t, pert[:,2], color='g')
 
 axs[3, 0].plot(t, netgain[3],color = 'orange', label = '$\Delta t = 600$')
 axs[3, 1].plot(t, Intensity[3],color = 'orange')
 axs[3, 0].text(1500, 2.5, '$\Delta t = 600$', fontsize = 10)
+axs[3, 1].plot(t, pert[:,3], color='g')
+
 plt.xlabel("Time (u.arb)")
 fig.suptitle('Net gain and Intensity for different perturbation time periods')
 plt.show()
