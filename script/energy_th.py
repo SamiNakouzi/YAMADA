e4666acc3ee32ba345a88c98a6cabb121bf80f25
Sami Nakouzi <sami_nakouzi@hotmail.com>
2022-04-12 15:58:27 +0200

    updated graph

--- script/energy_th.py
+++ script/energy_th.py
@@ -10,7 +10,7 @@ inv_dmu = []
 temp = []
 
 t = np.linspace(300, 600, 1000)
-dt = np.linspace(50 , 800, 24)
+dt = np.linspace(50 , 500, 50)
 
 model = yamada_model()
 
@@ -37,12 +37,12 @@ for i in range(len(temp)):
 #Ploting:
 
 style.use("seaborn")
-plt.scatter(temp, inv_dmu, color = 'r', label = "dots")
+plt.scatter(temp, inv_dmu, color = 'r', s = 30)
 plt.vlines(threashold, color = 'blue',ymin = min(inv_dmu), ymax = max(inv_dmu), ls = '--')
 plt.plot(temp, inv_dmu, '--k', alpha = 0.5)
 plt.xlabel('$\Delta t$')
 plt.ylabel('$\dfrac{1}{\delta \mu}$', rotation=0, fontsize=12, labelpad=20)
-plt.text(threashold, min(inv_dmu)-0.1, '$\Delta t = 278.26$')
-plt.legend()
+plt.text(threashold, min(inv_dmu)-0.1, '$\Delta t =$' + str("%.2f" % threashold))
+plt.text(100, max(inv_dmu) + 0.3, "$\mu1 = 2.43$")
 plt.title('$\dfrac{1}{\delta \mu} Vs \Delta t$')
 plt.show()
