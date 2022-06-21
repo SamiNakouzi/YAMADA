6999c564271d749011171bc97f0055153f57d32e
Sami Nakouzi <sami_nakouzi@hotmail.com>
2022-06-21 18:18:02 +0200

    bla

---
+++ script/pipe.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+#    for VARIABLE in {1..1000}
+#    do
+#        python3 snn_test.py
+#        python3 snn_model.py
+#    done
+
+   for VARLIABLE in {1..1000}
+   do
+       python3 pulses.py
+   done
